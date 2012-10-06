e46369501df5e46dec0e3946444b0c7f6cf81aa0
herney <herney400@gmail.com>
2012-10-05 20:03:24 -0500

    estado

--- src/modelo/Nodo.java
+++ src/modelo/Nodo.java
@@ -12,34 +12,43 @@ import java.awt.Point;
  */
 public class Nodo {
      int mundo[][];
+     
      Point posicionActual;
      int peso;
      Robot robot; 
      int pesoBasura=0;
      String ruta="";
      private int heuristica; 
+     Expandir espan;
      Estado estado;
      Nodo padre;
      int profundidad;
      int costoDelaRuta;
      String operadorAplicado;
+     
      /*en caso de que la heuristica la hagamos
       * con entero, que creo es lo mas viable...
      */
      
      
-     public Nodo(int mundoAmb[][], Point posicionActual, int peso){
-      this.mundo=mundoAmb;
-      this.posicionActual=posicionActual;
-      this.peso=peso;
+     public Nodo( Point posicionActual, int peso, String camino, int profundidad,int costoRut){
+       //this.mundo=mundoActual;
+       this.posicionActual=posicionActual;
+       this.peso=peso;
+       //this.robot=rob;
+       this.ruta=camino;
+       this.profundidad=profundidad;
+       this.costoDelaRuta=costoRut;
     }
+     
+     
     /*constructor 2*/ 
     public Nodo() {
     
     }
-    public void esMeta(){
-    
-    
+    public boolean esMeta(){
+    boolean meta=true;
+    return meta;
     }
     
     
@@ -55,6 +64,10 @@ public class Nodo {
     }
 
     
+
+    public void setMundo(int[][] mundo) {
+        this.mundo = mundo;
+    }
 
     public int[][] getMundo() {
         return mundo;
